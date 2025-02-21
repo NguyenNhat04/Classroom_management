5fc3b733add1487a8f5af50d00e7b68a41e614ba
tuanbao2 <fav0d0j2k3@gmail.com>
2025-02-21 13:53:36 +0700

    create register

--- server/src/models/login.model.js
+++ server/src/models/login.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const hash = require("sha256");
-const Configs = require('../../../configs/Constants');
 const ObjectId = Schema.Types.ObjectId;
 const LoginInfoSchema = new Schema({
     user_ref : {type: ObjectId, unique: true, ref: Configs.DB_SCHEMA.USER},
