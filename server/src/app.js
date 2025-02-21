ef3a7fa109fee6b072a47156449d4d083c1aaa36
nhatnguyendev <nhatnguyendev2604@gmail.com>
2025-02-21 13:48:48 +0700

    create error response , success response

--- server/src/app.js
+++ server/src/app.js
@@ -6,5 +6,14 @@ await mongoose.connect("mongodb://127.0.0.1/Classroom_Management");
 // init routes
 app.use("/", require("./routers/index"));
 
+// middleware handle error
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "error",
+    code: statusCode,
+    message: error.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
-                                                                                                                
